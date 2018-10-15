251bbd3cda6667034325d118f97497d44962fd87
Sean Kibbee <seankibbee@gmail.com>
2018-10-14 22:50:00 -0400

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,6 @@
+"""Entry point for the application."""
+from flask import render_template
+
+from . import app    # For application discovery by the 'flask' command.
+from . import views  # For import side-effects of setting up routes.
+
